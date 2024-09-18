04b34aa706496e8a572ff9c67dfd35e63e1fec8a
Tove-Larsson <144339361+Tove-Larsson@users.noreply.github.com>
2024-09-18 14:59:59 +0200

    Define Game entity

--- src/main/java/com/tove/ws_project/model/Game.java
+++ src/main/java/com/tove/ws_project/model/Game.java
@@ -1,6 +1,52 @@
 package com.tove.ws_project.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
 
+@Entity
 public class Game {
 
+    @Id
+    private Long id;
+    private String name;
+    private String storyline;
+    private double rating;
+
+    public Game() {
+    }
+
+    public Game(Long id, String name, String storyline, double rating) {
+        this.id = id;
+        this.name = name;
+        this.storyline = storyline;
+        this.rating = rating;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getStoryline() {
+        return storyline;
+    }
+
+    public void setStoryline(String storyline) {
+        this.storyline = storyline;
+    }
+
+    public double getRating() {
+        return rating;
+    }
+
+    public void setRating(double rating) {
+        this.rating = rating;
+    }
 }
